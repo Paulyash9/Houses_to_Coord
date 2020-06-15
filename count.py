4bb96416d85d41a66d12eb490cdf01cb17c93417
Pavel Malyshev <paulinio09@gmail.com>
2020-06-15 17:09:35 +0300

    Fixed bug

--- count.py
+++ count.py
@@ -14,13 +14,16 @@ def all_count():
     count = 0
     for a in range(len(data)):
         for lv1 in data[a].values():
-            for lv2 in lv1.values():
-                if type(lv2) is list:
-                    count += len(lv2)
-                else:
-                    for lv3 in lv2.values():
-                        if type(lv3) is list:
-                            count += len(lv3)
+            if type(lv1) is list:
+                count += len(lv1)
+            else:
+                for lv2 in lv1.values():
+                    if type(lv2) is list:
+                        count += len(lv2)
+                    else:
+                        for lv3 in lv2.values():
+                            if type(lv3) is list:
+                                count += len(lv3)
     return f'Всего адресов: {count}, городов/районов в {region}: {len(data)}'
 
 
@@ -29,7 +32,7 @@ def check_count():
     count = 0
     if End > len(data):
         raise ValueError(f'в {region} всего {len(data)} городов/районов. Уменьшите значение End')
-    for a in range(Start_from, End-1):
+    for a in range(Start_from, End - 1):
         for lv1 in data[a].values():
             for lv2 in lv1.values():
                 if type(lv2) is list:
@@ -45,4 +48,4 @@ if __name__ == '__main__':
     Start_from = 0
     End = 23
     print(check_count())
-    #print(all_count())
+    # print(all_count())
