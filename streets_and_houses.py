7d279b7ed4ac56ef8c84a62307791233e4077df1
Paulyash9 <paulinio09@gmail.com>
2021-03-31 12:40:59 +0300

    added async parse of addresses

--- streets_and_houses.py
+++ streets_and_houses.py
@@ -4,20 +4,21 @@ import requests as req
 import unicodedata as ud
 from bs4 import BeautifulSoup
 from settings import *
+from datetime import datetime
 
 
 def take_region():
-    response = req.get(url, headers=headers)
+    response = SESSION.get(base_url, headers=headers)
     region = BeautifulSoup(response.content, 'lxml').find('h1').text.split(', ')[0]
     return region
 
 
-def take_links(url=url):  # сбор ссылок со страницы
-    response = req.get(f'https://фиас.онлайн/{url}', headers=headers)
+def take_links(url=base_url):  # сбор ссылок со страницы
+    response = SESSION.get(f'https://фиас.онлайн{url}', headers=headers)
     soup = BeautifulSoup(response.content, 'lxml').find_all('div', class_="col-md-3 col-sm-6")
     links = dict()
     for i in soup:
-        links[ud.normalize("NFKD", i.find('a').text.replace(u'\xa0', u' '))] = i.find('a')['href'][1:]
+        links[ud.normalize("NFKD", i.find('a').text.replace(u'\xa0', u' '))] = i.find('a').get('href')
 
     if len(links.keys()) > 0:
         for key, value in links.items():
@@ -29,7 +30,7 @@ def take_links(url=url):  # сбор ссылок со страницы
 
 
 def take_houses(street):  # сбор домов со страницы
-    response = req.get(f'https://фиас.онлайн/{street}', headers=headers)
+    response = SESSION.get(f'https://фиас.онлайн{street}', headers=headers)
     soup = BeautifulSoup(response.content, 'lxml')
     soup = soup.find_all('a', href=True, rel=False, class_=False)
     houses = list()
@@ -40,17 +41,17 @@ def take_houses(street):  # сбор домов со страницы
 
 
 def first_parse():  # сбор ссылок со страницы
-    response = req.get(url, headers=headers)
+    response = SESSION.get(base_url, headers=headers)
     soup = BeautifulSoup(response.content, 'lxml').find_all('div', class_="col-md-3 col-sm-6")
     links = dict()
     for i in soup:
-        links[ud.normalize("NFKD", i.find('a').text.replace(u'\xa0', u' '))] = i.find('a')['href'][1:]
+        links[ud.normalize("NFKD", i.find('a').text.replace(u'\xa0', u' '))] = i.find('a').get('href')
     return links
 
 
 def check_file(settlement):
     try:
-        with open(Filename, 'r', encoding='utf-8') as file:
+        with open(FILENAME, 'r', encoding='utf-8') as file:
             opened_file = json.load(file)
         for i in opened_file:
             if settlement in i.keys():
@@ -58,7 +59,7 @@ def check_file(settlement):
                 return False
         return True
     except (FileNotFoundError, json.decoder.JSONDecodeError):
-        with open(Filename, 'w', encoding='utf-8') as file:
+        with open(FILENAME, 'w', encoding='utf-8') as file:
             file.close()
         return True
 
@@ -66,12 +67,12 @@ def check_file(settlement):
 def save_file(data, settlement):
     save_data = {settlement: data}
     try:
-        with open(Filename, 'r', encoding='utf-8') as file:
+        with open(FILENAME, 'r', encoding='utf-8') as file:
             exist_data = list(json.load(file))
     except json.decoder.JSONDecodeError:
         exist_data = list()
     exist_data.append(save_data)
-    with open(Filename, 'w', encoding='utf-8') as file:
+    with open(FILENAME, 'w', encoding='utf-8') as file:
         json.dump(exist_data, file, indent=4, ensure_ascii=False)
 
 
@@ -82,9 +83,11 @@ def count(completed, value):
 
 
 if __name__ == '__main__':
+    start = datetime.now()
     completed = 0
-    Region = take_region()
-    Filename = f'parsed {Region}.json'
+    SESSION = req.Session()
+    REGION = take_region()
+    FILENAME = f'parsed {REGION}.json'
 
     """сбор данных"""
     # 1. с области собираем ссылки на ГОРОДА и РАЙОНЫ
@@ -99,3 +102,5 @@ if __name__ == '__main__':
             completed = count(completed, len(data.keys()))
         else:
             completed = count(completed, len(data.keys()))
+    stop = datetime.now() - start
+    print(f"Выполнено за {stop}")
---
+++ streets_and_houses_async.py
@@ -0,0 +1,165 @@
+import aiohttp
+import asyncio
+import json
+import requests as req
+import unicodedata as ud
+from bs4 import BeautifulSoup
+from settings import *
+from datetime import datetime
+
+
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        result = func(*args, **kwargs)
+        print(f"Выполнено за {datetime.now() - start}")
+        return result
+
+    return wrapper
+
+
+def region_and_links() -> tuple:
+    session = req.Session()
+    soup = BeautifulSoup(session.get(base_url, headers=headers).content, 'html5lib')
+    region = soup.find('h1').text.split(', ')[0]
+
+    file = f'parsed {region}.json'
+    urls = get_links(soup)
+    url_list = urls.copy()
+    for settlement, settlement_url in urls.items():
+        if check_file(settlement, file):
+            url_list.pop(settlement)
+
+    return region, url_list
+
+
+def get_links(soup: BeautifulSoup) -> dict or bool:
+    urls = {}
+    for i in soup.find_all('div', class_="col-md-3 col-sm-6"):
+        urls.update({
+            ud.normalize("NFKD", i.find('a').text.replace(u'\xa0', u' ')):
+                'https://фиас.онлайн' + i.find('a').get('href')
+        })
+
+    return urls if len(urls) > 0 else False
+
+
+def get_houses(soup: BeautifulSoup) -> list or bool:
+    house = [i.text for i in soup.select("table.table:not(.table-striped.table-hover) "
+                                         "> tbody "
+                                         "> tr "
+                                         "> td:nth-child(2) "
+                                         "> a")]
+    return house if len(house) > 0 else False
+
+
+async def gather_and_parse(url_list: dict):
+    async with aiohttp.ClientSession() as session:
+        tasks = [asyncio.Task(fetch(session, key, url)) for key, url in url_list.items()]
+        await asyncio.gather(*tasks)
+    return tasks
+
+
+async def fetch(session, head, url: str = base_url):
+    scrapped_data = []
+    async with session.get(url, headers=headers) as response:
+        html = await response.text()
+    soup = BeautifulSoup(html, 'html5lib')
+
+    houses = get_houses(soup)
+    if houses:
+        scrapped_data.extend(houses)
+
+    # get list of links
+    links = get_links(soup)
+    if links:
+        scrapped_data.append(links)
+        for link in scrapped_data:
+            if isinstance(link, dict):
+                [link.update({key: await parsing(url)}) for key, url in link.items()]
+    print(f'{head} parsed')
+    return {head: scrapped_data}
+
+
+async def parsing(url: str) -> list:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as response:
+            html = await response.text()
+
+    soup = BeautifulSoup(html, 'html5lib')
+
+    scrapped_data = []
+    houses = get_houses(soup)
+    if houses:
+        scrapped_data.extend(houses)
+
+    # get list of links
+    links = get_links(soup)
+    if links:
+        scrapped_data.append(links)
+        for link in scrapped_data:
+            if isinstance(link, dict):
+                [link.update({key: await parsing(url)}) for key, url in link.items()]
+    return scrapped_data
+
+
+def check_file(settlement, file):
+    try:
+        with open(file, 'r', encoding='utf-8') as f:
+            opened_file = json.load(f)
+        for i in opened_file:
+            if settlement in i.keys():
+                print(f'{settlement} уже есть')
+                return True
+        return False
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        with open(file, 'w', encoding='utf-8') as f:
+            f.close()
+        return False
+
+
+def save_file(data, file):
+    exist_data = list()
+    try:
+        with open(file, 'r', encoding='utf-8') as f:
+            exist_data = list(json.load(f))
+    except (json.decoder.JSONDecodeError, FileNotFoundError):
+        exist_data = list()
+    exist_data.extend(data)
+    with open(file, 'w', encoding='utf-8') as f:
+        json.dump(exist_data, f, indent=4, ensure_ascii=False)
+
+
+@timeit
+def make_all():
+    # 1. preparing and checking exists data
+    region, url_list = region_and_links()  # get region name and list of first URLs
+    file = f'parsed {region}.json'
+
+    # 2. parsing
+    loop = asyncio.get_event_loop()
+    data = loop.run_until_complete(gather_and_parse(url_list))
+
+    # 3. cleaning
+    clean_data = []
+    for d in data:
+        clean_data.append(d.result())
+
+    # 4. saving
+    save_file(clean_data, file)
+
+
+if __name__ == '__main__':
+    make_all()
+
+""" 
+TEST # 1
+url = 'https://xn--80ap2aj.xn--80asehdb/4c2dd563-c3af-45ad-9a8f-2959de8383f0/' Кош-Агачский Район, Алтай респ
+Session(non-refactored): Выполнено за 0:01:34.670842
+Asyncio: Выполнено за 0:00:21.522011
+
+TEST # 2
+url = 'https://xn--80ap2aj.xn--80asehdb/c2deb16a-0330-4f05-821f-1d09c93331e6/' Санкт-Петербург Город
+Session(non-refactored): Выполнено за 0:21:43.494318
+Asyncio: Выполнено за 0:03:03.541377
+"""
